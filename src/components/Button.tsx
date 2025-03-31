ed3bff7684cf513c24639525f056c9614195c2b2
Pallavi <pallavishettigar03@gmail.com>
2025-03-31 14:24:38 +0530

    add new code

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,9 @@
 const Button = () => {
     return (
-      <button className="mt-6 bg-green-700 px-6 py-3 rounded-lg text-white text-lg hover:bg-green-700 flex items-center justify-center">
+      <button className="mt-6 bg-green-500 px-6 py-3 rounded-lg text-white text-lg hover:bg-green-800 flex items-center justify-center">
         <span></span> Generate List
       </button>
     );
   };
   
   export default Button;
-  
