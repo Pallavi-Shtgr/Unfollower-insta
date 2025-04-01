a4ec548e9718a0b67fc5cac76f5d14543c05bd75
Pallavi <pallavishettigar03@gmail.com>
2025-04-01 17:32:14 +0530

    add changes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL, // Ensure this is passed to your app during build
+  },
 };
-module.exports = nextConfig;
+
+export default nextConfig;
